0cecebd1818cf2836fc3979e995b99717318a467
Harley Hanna <harleyhannawrit@gmail.com>
2020-08-21 14:45:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -47,7 +47,7 @@ SELECT projects.category, pledges.amount
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-GROUP BY projects.category DESC
+GROUP BY projects.category
 WHERE projects.category = 'music';"
 end
 
